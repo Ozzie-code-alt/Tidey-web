793a6cb36aa4822294fec078ac7552f1e4c02e66
naey-bloom <naey@getbloom.io>
2026-09-16 02:47:51 +0800

    Move form to left and update CTA text wrap

--- components/home/ShortCTA.tsx
+++ components/home/ShortCTA.tsx
@@ -5,7 +5,7 @@ export default function ShortCTA() {
     <section className="bg-white p-5 lg:px-[clamp(60px,8.53vw,162px)] lg:py-[clamp(32px,3.16vw,60px)]">
       <div className="mx-auto flex w-full max-w-[1738px] flex-col items-center gap-[26px] rounded-[40px] px-5 py-5 lg:flex-row lg:justify-center lg:gap-[clamp(32px,3.16vw,60px)] lg:px-[clamp(40px,5.26vw,100px)]">
         <h2 className="max-w-[270px] text-center text-[24px] font-semibold leading-[30px] tracking-[-0.005em] text-[#00BAFA] lg:max-w-[clamp(257px,25.16vw,478px)] lg:text-[clamp(19px,1.89vw,36px)] lg:leading-[125%]">
-          Download Tidey now on iOS
+          Download Tidey<br className="md:block 2xl:hidden" /> now on iOS
         </h2>
         <div className="flex w-full max-w-[350px] flex-col gap-[14px] lg:w-auto lg:max-w-none lg:flex-row lg:gap-10">
           <StoreButton
